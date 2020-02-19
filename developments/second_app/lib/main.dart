d1859889dc9204b1ddac622396b748851f464bdb
simon t <satabligan@ucdavis.edu>
2020-02-19 15:37:25 -0800

    Customize personality results

--- developments/second_app/lib/main.dart
+++ developments/second_app/lib/main.dart
@@ -74,7 +74,7 @@ class _MyAppState extends State<MyApp> {
               questionIndex: _questionIndex,
               questions: _questions
             )
-            : Result(),
+            : Result(_totalScore),
             ),
       ),
     );
