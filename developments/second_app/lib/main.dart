c6cc614d7301b7d932dba24709677e53248fae73
simon t <satabligan@ucdavis.edu>
2020-02-18 17:55:44 -0800

    More compartmentalization: create Answers.dart widget file

--- developments/second_app/lib/main.dart
+++ developments/second_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart'
 
 import './question.dart';
+import './answer.dart'
 
 void main() {
   runApp(MyApp());
@@ -38,18 +39,9 @@ class _MyAppState extends State<MyApp> {
             Question(
               questions[_questionIndex],
             ),
-            RaisedButton(
-                child: Text('Answer 1'),
-                onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-                child: Text('Answer 2'),
-                onPressed: () => print("Answer 2 chosen"),
-            ),
-            RaisedButton(
-                child: Text('Answer 3'),
-                onPressed: _answerQuestion,
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
