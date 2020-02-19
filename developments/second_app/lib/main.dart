aea8743951d994ed585f09d9ee7b41a0fb779f22
simon t <satabligan@ucdavis.edu>
2020-02-19 14:07:31 -0800

    Make questions constant

--- developments/second_app/lib/main.dart
+++ developments/second_app/lib/main.dart
@@ -27,7 +27,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
+    const questions = const [
       {
         'questionText' : 'What\'s your favorite color?',
         'answers': ['Black', 'Red', 'Green', 'White'],
